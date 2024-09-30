e841d4ac31ed4d47fd84061127c00300d5f6f382
ooooose <yuuki.oose1990@gmail.com>
2024-10-01 06:51:08 +0900

    Fix エラー解消

--- app/controllers/concerns/jwt_authenticatable.rb
+++ app/controllers/concerns/jwt_authenticatable.rb
@@ -3,17 +3,15 @@ module JwtAuthenticatable
 
   SECRET_KEY = Rails.application.credentials.secret_key_base
 
-  class_methods do
-    def encode(payload)
-      JWT.encode(payload, SECRET_KEY, "HS256")
-    end
+  def encode(payload)
+    JWT.encode(payload, SECRET_KEY, "HS256")
+  end
 
-    def decode(token)
-      decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: "HS256" })[0]
-      HashWithIndifferentAccess.new decoded
-    rescue JWT::DecodeError => e
-      Rails.logger.error "JWT decode error: #{e.message}"
-      nil
-    end
+  def decode(token)
+    decoded = JWT.decode(token, SECRET_KEY, true, { algorithm: "HS256" })[0]
+    HashWithIndifferentAccess.new decoded
+  rescue JWT::DecodeError => e
+    Rails.logger.error "JWT decode error: #{e.message}"
+    nil
   end
 end
