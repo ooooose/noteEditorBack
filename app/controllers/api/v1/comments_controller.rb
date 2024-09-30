6aafa7ed6b51d36f2449c68c90df110f1b8f19d0
ooooose <yuuki.oose1990@gmail.com>
2024-10-01 06:41:07 +0900

    Fix コントローラ内の記述を修正

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::CommentsController < ApplicationController
-  picture = Picture.find(params[:picture_id])
-  @comments = picture.comments
+  def index
+    picture = Picture.find(params[:picture_id])
+    @comments = picture.comments
 
-  render json: @comments, each_serializer: CommentSerializer
+    render json: @comments, each_serializer: CommentSerializer
+  end
 end
