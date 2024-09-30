7e5132d4c5da9675df3986d38c214bf015dd5ec2
ooooose <yuuki.oose1990@gmail.com>
2024-10-01 07:28:32 +0900

    Fix エラー解消

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,13 +42,15 @@ RSpec.describe User, type: :model do
   # クラスメソッドのテスト
   describe ".find_with_jwt" do
     let!(:user) { create(:user) }
-    let!(:secret_key) { Rails.application.secrets.secret_key_base }
+    let!(:secret_key) { Rails.application.credentials.secret_key_base }
     let!(:token) do
       JWT.encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, secret_key, "HS256")
     end
 
     context "when the token is valid" do
       it "finds a user" do
+        puts "token: #{token}"
+        puts "user: #{user}"
         expect(described_class.find_with_jwt(token)).to eq user
       end
     end
