0222a8e5e13cf767eca1016d6afc9c0ee5861dc7
ooooose <yuuki.oose1990@gmail.com>
2024-09-30 23:52:09 +0900

    Fix application.rbにservice層を読み込ませる

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module App
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Tokyo"
+    config.autoload_paths << Rails.root.join('app/services')
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Only loads a smaller set of middleware suitable for API only apps.
