f899e714ddd280addfaf7ec022785ec460200ee1
ooooose <yuuki.oose1990@gmail.com>
2024-10-01 19:18:33 +0900

    Add コントローラの作成

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -2,6 +2,19 @@ class Api::V1::LikesController < ApplicationController
 
   # POST /api/v1/likes
   def create
-    current_user.likes.create!(picture_id: params[:picture_id])
+    current_user.like(@picture)
+    return render json: { message: "Liked the picture" }, status: :ok
+  end
+
+  # DELETE /api/v1/likes/:id
+  def destroy
+    current_user.unlike(@picture)
+    return render json: { message: "Unliked the picture" }, status: :ok
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.find(params[:picture_id])
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < ApplicationController
   include JwtAuthenticatable
   skip_before_action :authenticate_request, only: %i[create]
 
+  # POST /api/v1/users
   def create
     @current_user = User.find_by(email: user_params[:email])
 
@@ -20,7 +21,23 @@ class Api::V1::UsersController < ApplicationController
     render json: { error: "ログインに失敗しました: #{e.message}" }, status: :internal_server_error
   end
 
+  # GET /api/v1/users/:id
+  def show
+    render json: @current_user, status: :ok
+  end
+
+  # DELETE /api/v1/users/:id
+  def destroy
+    @current_user.soft_destroy
+    render json: { message: "ユーザーを削除しました" }, status: :ok
+  rescue => e
+    render json: { error: "ユーザーの削除に失敗しました: #{e.message}" }, status: :internal_server_error
+  end
+
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     def user_params
       params.require(:user).permit(:email)
