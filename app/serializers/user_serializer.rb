e7ad15b7f394e4094a6ed38d2980cfeb25e3b4fa
ooooose <yuuki.oose1990@gmail.com>
2024-10-31 22:31:26 +0900

    Add 必要なエンドポイントの作成

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
   set_type :user
-  attributes :name, :email, :image
+  attributes :name, :email, :image, :uid
 
   has_many :pictures, serializer: PictureSerializer
   has_many :liked_pictures, serializer: PictureSerializer
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,23 @@ Rails.application.routes.draw do
   post 'auth/:provider/callback', to: 'api/v1/users#create'
   namespace :api, format: 'json' do
     namespace :v1 do
-      resources :users, only: %i[create show destroy] do
+      resources :users, only: %i[create show destroy], param: :uid do
         collection do
           get 'me', to: 'users#me'
           put 'profile', to: 'users#update_profile'
         end
+        
+        member do
+          get 'pictures', to: 'users#pictures'
+          get 'liked_pictures', to: 'users#liked_pictures'
+        end
       end
+
       resources :themes, only: %i[index show create update destroy]
       resources :pictures, only: %i[index create update destroy] do
         resources :comments, only: %i[index show create update destroy]
       end
+
       resources :likes, param: :picture_uid, only: %i[create destroy]
     end
   end
