95e8e86022b8f589b763a03b74917c85c26c06cb
ooooose <yuuki.oose1990@gmail.com>
2024-11-02 17:58:21 +0900

    Add RSpec追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,13 +5,7 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /api/v1/users/profile
   def me
-    render json: UserSerializer.new(
-      current_user,
-      include: [
-        :pictures, :'pictures.user', :'pictures.likes', :'pictures.comments',
-        :liked_pictures, :'liked_pictures.user', :'liked_pictures.likes', :'liked_pictures.comments'
-      ]
-    ).serializable_hash, status: :ok
+    render json: UserSerializer.new(current_user).serializable_hash, status: :ok
   end
 
   # PATCH /api/v1/users/profile
--- spec/requests/api/v1/themes_spec.rb
+++ spec/requests/api/v1/themes_spec.rb
@@ -1,7 +1,31 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::Themes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+  let!(:token) { encode_jwt({ user_id: user.id }) }
+  let!(:headers) { { Authorization: "Bearer #{token}" } }
+
+  describe "GET /api/v1/themes" do
+    let!(:themes) { create_list(:theme, 3) }
+
+    context "when the user gets the themes" do
+      before { get api_v1_themes_path, headers: headers }
+
+      it "returns status ok" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "returns themes" do
+        expect(JSON.parse(response.body)["data"].length).to eq(3)
+      end
+    end
+
+    context "when unauthorized" do
+      before { get api_v1_themes_path }
+
+      it "returns status unauthorized" do
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
   end
 end
